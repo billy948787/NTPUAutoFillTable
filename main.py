22c9efbd6627459616b24a314f774e854d56da96
billy948787 <88420168+billy948787@users.noreply.github.com>
2024-01-21 23:40:12 +0800

    final

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver import  ActionChains
-from selenium.webdriver.common.keys import  Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
+import pyperclip as pc
 import time
-import os
+from os import _exit
+
+
+print('請選擇系統使用的瀏覽器(請輸入整數!),1:Chrome 2:Edge 3:Firefox')
+
+try :
+    browerType = eval(input())
+except :
+    print('輸入為非整數,程式將停止')
+    time.sleep(1)
+    _exit(0)
+
+if browerType == 1:
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+elif browerType == 2:
+    driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+elif browerType == 3:
+    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+else:
+    print('輸入為未支援瀏覽器，程式將停止')
+    time.sleep(1)
+    _exit(0)
 
 
 
-
-driver = webdriver.Edge('./msedgedriver')
 def main() :
+
     driver.get(r"https://ohs02.ntpu.edu.tw/student_new.htm");
 
-    # account = input()
-    # password = input()
-    account = '411285040'
-    password = 'H70471215'
+    print('請輸入帳號:')
+    account = input()
+    print('請輸入密碼:')
+    password = input()
 
     accountInput = driver.find_element_by_xpath("/html/body/table/tbody/tr/td[1]/table[3]/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input")
     passwordInput = driver.find_element_by_xpath("/html/body/table/tbody/tr/td[1]/table[3]/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr[3]/td/input")
     loginButton = driver.find_element_by_xpath('''//*[@id="loginBtn1"]''')
 
-    ActionChains(driver)\
-        .send_keys_to_element(accountInput, account)\
-        .send_keys_to_element(passwordInput, password)\
-        .click(loginButton)\
-        .perform()
+    pc.copy(account)
+    accountInput.send_keys(Keys.CONTROL, 'v')    
+    pc.copy(password)
+    passwordInput.send_keys(Keys.CONTROL, 'v')    
+
+    loginButton.click()
+
 
     driver.get(r"https://ohs02.ntpu.edu.tw/pls/univer/g0002.eval_form.sel_course?date1=2024012165850")
 
@@ -38,11 +64,9 @@ def startLoop():
         col = row.find_elements_by_tag_name(name='td')
         for element in col:
             try : 
-                print('Can click',element.text)
                 link = element.find_element_by_tag_name(name='a')
                 href = link.get_attribute("href")
                 driver.get(href)
-                print(href)
                 time.sleep(2)
             except : 
                 continue
@@ -53,53 +77,33 @@ def startLoop():
 def fillSheet():
 
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[1]/tbody/tr[4]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[1]/tbody/tr[4]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[4]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[4]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[5]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[5]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[6]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[6]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[7]/th[6]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[7]/th[6]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[8]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[8]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[9]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[9]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[10]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[10]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[11]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[11]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[12]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[12]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[13]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[13]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[14]/th[6]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[14]/th[6]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[15]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[15]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[16]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[16]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[17]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[17]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[22]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[22]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[23]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[23]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[24]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[24]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[26]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[26]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[27]/th[2]/input').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[27]/th[2]/input').send_keys(Keys.space)
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[28]/th[1]/div/input[2]').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[28]/th[1]/div/input[2]').send_keys(Keys.space)
+    # drive
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[29]/th[1]/div/input[1]').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[29]/th[1]/div/input[1]').send_keys(Keys.space)
+    # drive
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[30]/th[1]/div/input[2]').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[30]/th[1]/div/input[2]').send_keys(Keys.space)
+    # drive
     driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[31]/th[1]/div/input[3]').click()
-    # driver.find_element_by_xpath('/html/body/center/form/center[1]/table[2]/tbody/tr[31]/th[1]/div/input[3]').send_keys(Keys.space)
+    # drive
         
 
 
@@ -111,9 +115,8 @@ def sentSheet():
     
 
 
-
-
 if __name__ == '__main__':
     main()
-    time.sleep(5)
+    print("已成功填寫!")
     driver.close()
+    _exit(0)
